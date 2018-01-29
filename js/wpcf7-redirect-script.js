7ddee2715a5eed02ecde848def329559b5338542
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2018-01-29 12:53:44 +0100

    Add Do not Send and Multisite Relative url options (Prettify)

--- js/wpcf7-redirect-script.js
+++ js/wpcf7-redirect-script.js
@@ -12,11 +12,11 @@ function wpcf7_redirect_mailsent_handler() {
 		jQuery.post(form.ajaxurl, data, function (is_multisite) {
 			var url = window.location.protocol + window.location.hostname;
 			var port = window.location.port;
-		// Script to run after sent.
-		if ( form.after_sent_script ) {
-			eval( form.after_sent_script );
-		}
-	
+			// Script to run after sent.
+			if ( form.after_sent_script ) {
+				eval( form.after_sent_script );
+			}
+
 
 			if (is_multisite == "TRUE" && form.use_relative_url) {
 				if (form.use_relative_url && form.relative_url || form.use_relative_url && form.relative_url && form.use_external_url) {
@@ -34,39 +34,39 @@ function wpcf7_redirect_mailsent_handler() {
 					}
 				}
 			} else {
-		// Redirect to external URL.
-		if ( form.use_external_url && form.external_url ) {
-			if ( form.http_build_query ) {
-				// Build http query
-				http_query = jQuery.param( event.detail.inputs, true );
-				form.external_url = form.external_url + '?' + http_query;
+			// Redirect to external URL.
+			if ( form.use_external_url && form.external_url ) {
+				if ( form.http_build_query ) {
+					// Build http query
+					http_query = jQuery.param( event.detail.inputs, true );
+					form.external_url = form.external_url + '?' + http_query;
+				}
+
+				if ( ! form.open_in_new_tab ) {
+					// Open in current tab
+					location.href = form.external_url;
+				} else {
+					// Open in external tab
+					window.open( form.external_url );
+				}
 			}
 
-			if ( ! form.open_in_new_tab ) {
-				// Open in current tab
-				location.href = form.external_url;
-			} else {
-				// Open in external tab
-				window.open( form.external_url );
-			}
-		}
-		
-		// Redirect to a page in this site.
-		else if ( form.thankyou_page_url ) {
-			if ( form.http_build_query ) {
-				// Build http query
-				http_query = jQuery.param( event.detail.inputs, true );
-				form.thankyou_page_url = form.thankyou_page_url + '?' + http_query;
-			}
+			// Redirect to a page in this site.
+			else if ( form.thankyou_page_url ) {
+				if ( form.http_build_query ) {
+					// Build http query
+					http_query = jQuery.param( event.detail.inputs, true );
+					form.thankyou_page_url = form.thankyou_page_url + '?' + http_query;
+				}
 
-			if ( ! form.open_in_new_tab ) {
-				// Open in current tab
-	    		location.href = form.thankyou_page_url;
-	    	} else {
-	    		// Open in new tab
-	    		window.open( form.thankyou_page_url );
-	    	}
-		}
+				if ( ! form.open_in_new_tab ) {
+					// Open in current tab
+					location.href = form.thankyou_page_url;
+				} else {
+					// Open in new tab
+					window.open( form.thankyou_page_url );
+				}
+			}
 
 	}, false );
 	});
